ab5eec59b64d5eec8676a3f8d05d5b226263fa22
Masterchef365 <duncan.freeman1@gmail.com>
2025-04-26 22:24:01 -0700

    Indexing shenanigans

--- src/generic_pyndarray.rs
+++ src/generic_pyndarray.rs
@@ -1,7 +1,7 @@
 use ndarray::{ArrayViewD, ArrayViewMutD, IxDyn, SliceInfo, SliceInfoElem};
 use rustpython_vm::{
     atomic_func,
-    builtins::{PyInt, PyModule, PySlice, PyStr, PyTuple},
+    builtins::{PyInt, PyModule, PyNone, PySlice, PyStr, PyTuple},
     class::PyClassImpl,
     convert::ToPyObject,
     protocol::PyMappingMethods,
@@ -136,15 +136,20 @@ impl<T: Display> Display for PyNdArray<T> {
 pub fn pyint_to_isize(int: &PyInt, vm: &VirtualMachine) -> PyResult<isize> {
     int.as_bigint()
         .try_into()
-        .map_err(|e| vm.new_runtime_error(format!("{e}")))
+        .map_err(|e| vm.new_runtime_error(format!("Bigint cast {e}")))
 }
 
 /// Converts a PyObject to an isize
-pub fn py_obj_elem_to_isize(obj: &PyObject, vm: &VirtualMachine) -> PyResult<isize> {
+pub fn py_obj_elem_to_isize(obj: &PyObject, vm: &VirtualMachine) -> PyResult<Option<isize>> {
+    if obj.downcast_ref::<PyNone>().is_some() {
+        return Ok(None);
+    }
+
     let int: &PyInt = obj
         .downcast_ref::<PyInt>()
         .ok_or_else(|| vm.new_runtime_error("Indices must be isize".to_string()))?;
-    pyint_to_isize(int, vm)
+
+    pyint_to_isize(int, vm).map(Some)
 }
 
 /// Converts a PyObject to a SliceInfoElem
@@ -152,35 +157,40 @@ pub fn py_index_elem_to_sliceinfo_elem(
     elem: PyObjectRef,
     vm: &VirtualMachine,
 ) -> PyResult<SliceInfoElem> {
+    //dbg!(&elem);
+
     if let Some(int) = elem.downcast_ref::<PyInt>() {
         return Ok(SliceInfoElem::Index(pyint_to_isize(int, vm)?));
     }
 
     if let Some(slice) = elem.downcast_ref::<PySlice>() {
+        eprintln!("stop");
         let stop = py_obj_elem_to_isize(&slice.stop, vm)?;
+        eprintln!("start");
         let start = slice
             .start
             .as_ref()
-            .map(|start| py_obj_elem_to_isize(start, vm))
+            .and_then(|start| py_obj_elem_to_isize(start, vm).transpose())
             .transpose()?;
+        eprintln!("step");
         let step = slice
             .step
             .as_ref()
-            .map(|step| py_obj_elem_to_isize(step, vm))
+            .and_then(|step| py_obj_elem_to_isize(step, vm).transpose())
             .transpose()?;
         return Ok(SliceInfoElem::Slice {
-            start: start.unwrap_or(stop),
+            start: start.unwrap_or(0),
             step: step.unwrap_or(1),
-            end: Some(stop),
+            end: stop,
         });
     }
 
-    Err(vm.new_runtime_error("Unrecognized index {elem:?}".to_string()))
+    Err(vm.new_runtime_error(format!("Unrecognized index {elem:?}")))
 }
 
 /// Converts a PyObject to a DynamicSlice
 pub fn py_index_to_sliceinfo(shape: PyObjectRef, vm: &VirtualMachine) -> PyResult<DynamicSlice> {
-    if let Ok(single) = py_index_elem_to_sliceinfo_elem(shape.clone(), vm) {
+    if let Ok(single) = dbg!(py_index_elem_to_sliceinfo_elem(shape.clone(), vm)) {
         return Ok(DynamicSlice::try_from(vec![single]).unwrap());
     }
 
@@ -192,7 +202,7 @@ pub fn py_index_to_sliceinfo(shape: PyObjectRef, vm: &VirtualMachine) -> PyResul
         return Ok(DynamicSlice::try_from(indices).unwrap());
     }
 
-    Err(vm.new_runtime_error("Unrecognized index {shape:?}".to_string()))
+    Err(vm.new_runtime_error(format!("Unrecognized sliceinfo index {shape:?}")))
 }
 
 /// Converts a PyObject shape to a Vec<usize>
